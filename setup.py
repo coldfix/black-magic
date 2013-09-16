226cbddbe2f433e1182a32f20aae4902474d87c7
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-17 00:04:11 +0200

    Add setup.py for distribution with setuptools

---
+++ setup.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+from distutils.core import setup
+from distutils.spawn import find_executable
+
+# Generate .rst document for PyPI
+try:
+    from pypandoc import convert
+    convert('README.md', 'rst')
+    with open('README.rst', 'wt') as f:
+        f.write(long_description)
+except ImportError:
+    long_description = None
+
+# invoke distutils
+setup(
+    name='black-magic',
+    version='0.0.1',
+    description='Decorator utility that operates on black magic',
+    long_description=long_description,
+    author='Thomas Gläßle',
+    author_email='t_glaessle@gmx.de',
+    url='https://github.com/coldfix/black-magic',
+    license='Public Domain',
+    packages=[
+        'black_magic',],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    ],
+)
+
