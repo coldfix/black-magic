fbf23349c6badcaf99758ca8bc662f440d471c74
Thomas Gläßle <t_glaessle@gmx.de>
2013-10-03 14:05:37 +0200

    Improve tests
    - one test function for each signature type
    - better code readability
    - hopefully better assertion error messages (through code structure)

--- test/py3/test_decorator.py
+++ test/py3/test_decorator.py
@@ -2,21 +2,64 @@
 """
 Python3 unit tests for black_magic.decorator
 """
+__all__ = ['TestASToratorPy3']
 
 import unittest
-import test.test_decorator
+from test.test_decorator import Util, hd
 
-class TestASTorator(test.test_decorator.TestASTorator):
+class TestASToratorPy3(unittest.TestCase, Util):
+    """
+    Python3 only tests for black_magic.decorator.wraps (uses AST).
 
-    def setUp(self):
-        self.sample_fns = dict(
-            kwonly  = lambda *, a, b=1: a+2*b,
-            combined = lambda a,b=1,*args,c,d=1,**kwargs: a+2*b+3*sum(args)+4*sum(kwargs.values())+5*c+6*d,
-            )
+    Contains checks for annotations object identity and keyword-only
+    arguments.
 
-        self.sample_args = dict(
-            kwonly  = [ ([],{'a':3, 'b':1}), ([],{'a':3}) ],
-            combined = [ ([1,2,3],{'c':4}), ([1],{'c':2, 'kw':4}),
-                        ([],{'a':1,'b':2,'c':4}) ]
-            )
+    """
+    def test_annotations_identity(self):
+        """Check that annatotions object identity is preserved."""
+        _a, _b, _c, _d, _va, _kw, _return = [], [], [], [], [], [], []
+        def real(a:_a, b:_b=1, *va:_va, c:_c, d:_d=1, **kw:_kw) -> _return:
+            pass
+        self.mutate(real)
+        _ = self.fake.__annotations__
+        self.assertIs(_['a'], _a)
+        self.assertIs(_['b'], _b)
+        self.assertIs(_['c'], _c)
+        self.assertIs(_['d'], _d)
+        self.assertIs(_['va'], _va)
+        self.assertIs(_['kw'], _kw)
+        self.assertIs(_['return'], _return)
+
+    def test_kwonly_arguments(self):
+        """Test function with only keyword-only arguments."""
+        def real(*, c, d=1):
+            return hash((c,d))
+        self.mutate(real)
+        self.check_result(c=0)
+        self.check_result(c=0, d=1)
+        self.must_fail()
+        self.must_fail(0)
+        self.must_fail(0, 1)
+        self.must_fail(d=1)
+
+    def test_all_argument_kinds(self):
+        """Test function with all kinds of (python3) arguments."""
+        def real(a, b=1, *args, c, d=1, **kwargs):
+            return hash((a,b,args,c,d,hd(kwargs)))
+        self.mutate(real)
+        self.check_result(0, c=4)
+        self.check_result(a=0, c=4)
+        self.check_result(0, 1, c=4)
+        self.check_result(0, b=1, c=4)
+        self.check_result(0, 1, 2, 3, c=4)
+        self.check_result(0, c=4, d=5)
+        self.check_result(0, c=4, d=5, e=6)
+        self.check_result(0, 1, 2, 3, c=4, d=5, e=6)
+        self.must_fail()
+        self.must_fail(0)
+        self.must_fail(c=4)
+        self.must_fail(a=0)
+        self.must_fail(0, 4)
+        self.must_fail(0, 4, b=1)
+
 
--- test/test_decorator.py
+++ test/test_decorator.py
@@ -1,47 +1,134 @@
 # encoding: utf-8
 """
-Python 2-3 unit tests for black_magic.decorator
-
-Currently, the only available tests are for decompile_argspec.
-
+Python2 compatible unit tests for black_magic.decorator
 """
+__all__ = ['TestASTorator']
 
 import unittest
 from black_magic.decorator import wraps
-from black_magic.compat import getfullargspec
+from black_magic.compat import getfullargspec, signature
 
 
-class TestASTorator(unittest.TestCase):
-    def setUp(self):
-        self.sample_fns = dict(
-                no_args  = lambda: 0,
-                pos_args = lambda a: a,
-                def_args = lambda a,b=2: a+2*b,
-                varargs  = lambda *args: sum(args),
-                kwargs   = lambda **kwargs: sum(kwargs.values()),
-                combined = lambda a,b=1,*args,**kwargs: a+2*b+3*sum(args)+4*sum(kwargs.values()),
-                )
+class Util(object):
+    def mutate(self, real):
+        @wraps(real)
+        def fake(*args, **kwargs):
+            return ~real(*args, **kwargs)
+        self.assertEqual(signature(fake), signature(real))
+        self.assertEqual(getfullargspec(fake), getfullargspec(real))
+        self.real = real
+        self.fake = fake
 
-        self.sample_args = dict(
-            no_args  = [ ([],{}) ],
-            pos_args = [ ([7],{}), ([], {'a':7}) ],
-            def_args = [ ([9,2],{}), ([],{'a':9,'b':2}), ([9],{}), ([9],{'b':2}) ],
-            varargs  = [ ([5,2],{}), ([2,5],{}) ],
-            kwargs   = [ ([],{'a':7, 'b':3}), ([],{'b':3,'c':7}) ],
-            combined = [ ([1,2,3],{'kw':4}), ([1],{'b':2, 'kw':4}),
-                        ([],{'a':1,'b':2,'kw':4}) ]
-            )
+    def check_result(self, *args, **kwargs):
+        self.assertEqual(~self.real(*args, **kwargs),
+                         self.fake(*args, **kwargs))
 
-    def test_argspec(self):
-        for case in self.sample_fns:
-            fn = self.sample_fns[case]
-            fake = wraps(fn)(lambda *args,**kwargs: 1+fn(*args, **kwargs))
-            assert getfullargspec(fake) == getfullargspec(fn), "for %s()" % case
+    def must_fail(self, *args, **kwargs):
+        try:
+            self.fake(*args, **kwargs)
+        except TypeError:
+            pass
+        else:
+            assert False
 
-    def test_return_value(self):
-        for case in self.sample_fns:
-            fn = self.sample_fns[case]
-            fake = wraps(fn)(lambda *args,**kwargs: 1+fn(*args, **kwargs))
-            for args,kwargs in self.sample_args[case]:
-                assert fake(*args, **kwargs) == 1 + fn(*args,**kwargs), 'While calling %s(*%r, **%r)' % (case, args, kwargs)
+def hd(d):
+    """Get hashable form of dictionary."""
+    return frozenset(d.items())
+
+
+class TestASTorator(unittest.TestCase, Util):
+    """
+    Unit tests for the wraps function (uses ASTorator).
+
+    Contains only python2 compatible tests, which means no keyword-only
+    arguments, no annotations.
+
+    """
+    def test_without_arguments(self):
+        """Test function without arguments."""
+        def real():
+            return hash(())
+        self.mutate(real)
+        self.check_result()
+        self.must_fail(0)
+        self.must_fail(a=0)
+
+    def test_positional_arguments(self):
+        """Test function with only positional arguments."""
+        def real(a, b=1):
+            return hash((a,b))
+        self.mutate(real)
+        self.check_result(0)
+        self.check_result(0, 1)
+        self.check_result(0, b=1)
+        self.check_result(a=0)
+        self.check_result(a=0, b=1)
+        self.must_fail()
+        self.must_fail(b=1)
+        self.must_fail(a=0, b=1, c=2)
+        self.must_fail(0, 1, 2)
+
+    def test_varargs(self):
+        """Test function with only variable length positional arguments."""
+        def real(*args):
+            return hash((args))
+        self.mutate(real)
+        self.check_result()
+        self.check_result(0)
+        self.check_result(0, 1)
+        self.check_result(0, 1, 2)
+        self.must_fail(a=0)
+        self.must_fail(0, b=1)
+
+    def test_kwargs(self):
+        """Test function with only variable length keyword arguments."""
+        def real(**kwargs):
+            return hash((hd(kwargs)))
+        self.mutate(real)
+        self.check_result()
+        self.check_result(a=0)
+        self.check_result(a=0, b=1)
+        self.check_result(a=0, b=1, c=2)
+        self.must_fail(0)
+        self.must_fail(0, 1)
+
+    def test_all_argument_kinds(self):
+        """Test function with all (python2) kinds of arguments."""
+        def real(a, b=1, *args, **kwargs):
+            return hash((a,b,args,hd(kwargs)))
+        self.mutate(real)
+        self.check_result(0)
+        self.check_result(0, 1)
+        self.check_result(0, 1, 2)
+        self.check_result(a=0)
+        self.check_result(a=0, b=1)
+        self.check_result(a=0, b=1, c=1)
+        self.check_result(0, 1, 2, d=1)
+        self.must_fail(b=1)
+        self.must_fail(b=1, c=1)
+
+    def test_default_value_identity(self):
+        """Check that object identity is conserved for default values."""
+        x = []
+        def real(a = x):
+            return a
+        @wraps(real)
+        def fake(*args, **kwargs):
+            return real(*args, **kwargs)
+        self.assertIs(fake(), x)
+        self.assertIsNot(fake([]), x)
+
+    def test_lambda(self):
+        """Test that wrapping works also on a lambda."""
+        real = lambda a, b=1, *args, **kwargs: hash((a,b,args,hd(kwargs)))
+        self.mutate(real)
+        self.check_result(0)
+        self.check_result(0, 1)
+        self.check_result(0, 1, 2)
+        self.check_result(a=0)
+        self.check_result(a=0, b=1)
+        self.check_result(a=0, b=1, c=1)
+        self.check_result(0, 1, 2, d=1)
+        self.must_fail(b=1)
+        self.must_fail(b=1, c=1)
 
