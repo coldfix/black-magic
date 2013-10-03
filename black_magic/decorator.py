de565848af9e45ec56c4d971d38a28c14bb04379
Thomas Gläßle <t_glaessle@gmx.de>
2013-10-03 04:00:56 +0200

    Drop string based decorator in favor of AST based decorator

--- black_magic/decorator.py
+++ black_magic/decorator.py
@@ -1,94 +1,177 @@
 """
-Decorator utility for function decorators.
+Decorator utility for function decorators that works using ast.
 
 This is intented to be a utility for signature preserving function
 decorators.
 
 """
-__all__ = [
-    'decompile_argspec',
-    'wraps',
-    'function_decorator',
-]
+from __future__ import absolute_import
 
+__all__ = ['ASTorator']
+
+import ast
+import inspect
 import functools
-from .compat import getfullargspec
-from .common import param_names, Scope
+
+from .compat import signature, ast_arg, exec_compat, is_identifier
+from .common import Scope
 
 
-def decompile_argspec(argspec, defparam_name):
+class ASTorator(object):
     """
-    Decompile a function argspec as returned by inspect.getfullargspec().
+    Decorates wapper functions with a given function signature.
 
-    Returns a tuple (declaration, forward, context), where
-    
-        - declaration: parameter list as found in the function definition
-        - forward: values of the parameters for forwarding to this function
-        - context: dictionary of default parameters used in the declaration
-
-    Example:
-
-    >>> spec = getfullargspec(lambda a, b=1, *args, **kwargs: None)
-    >>> decompile_argspec(spec, '_')
-    ('a, b=_[0], *args, **kwargs', 'a, b, *args, **kwargs', {'_': [1]})
-
-    >>> spec = getfullargspec(lambda a, b: None)
-    >>> decompile_argspec(spec, '_p')
-    ('a, b', 'a, b', {'_p': []})
+    Uses AST not eval.
 
     """
-    ctx = { defparam_name: [] }
-    decl_params = []
-    call_params = []
+    def __init__(self, signature, funcname=None, filename=None):
+        self.signature = signature
+        self.funcname = funcname
+        self.filename = filename
 
-    # Add a value to the context
-    def set_value(value):
-        ctx[defparam_name].append(value)
-        return '%s[%d]' % (defparam_name, len(ctx[defparam_name]) - 1)
-    
-    # Add parameter
-    def add_param(decl, call):
-        decl_params.append(decl)
-        call_params.append(call)
-
-    # start by adding the positional arguments without defaults:
-    defaults = argspec.defaults or []
-    num_required = len(argspec.args) - len(defaults)
-    decl_params += argspec.args[:num_required]
-    call_params += argspec.args[:num_required]
-
-    # positional arguments with defaults:
-    for arg,val in zip(argspec.args[num_required:], defaults):
-        add_param("%s=%s" % (arg, set_value(val)), arg)
-
-    # add varargs:
-    if argspec.varargs:
-        add_param('*'+argspec.varargs, '*'+argspec.varargs)
-    # need this if no varargs are given but keywordonly args are present:
-    elif argspec.kwonlyargs:
-        decl_params.append('*')
-
-    # now add keyword-only arguments (this is specific to python3)
-    for kwonlyarg in argspec.kwonlyargs:
-        if kwonlyarg in argspec.kwonlydefaults:
-            val = argspec.kwonlydefaults[kwonlyarg]
-            add_param("%s=%s" % (kwonlyarg, set_value(val)),
-                      "%s=%s" % (kwonlyarg, kwonlyarg))
+    @classmethod
+    def from_function(cls, function):
+        try:
+            filename = inspect.getsourcefile(function)
+        except:
+            filename = None
+        if is_identifier(function.__name__):
+            funcname = function.__name__
         else:
-            add_param(kwonlyarg,
-                      "%s=%s" % (kwonlyarg, kwonlyarg))
+            funcname = None
+        return cls(signature(function), funcname, filename)
 
-    # and finally the keyword arguments
-    if argspec.varkw is not None:
-        add_param('**'+argspec.varkw, '**'+argspec.varkw)
+    def decorate(self, callback):
+        """
+        Create wrapper that calls function with all its arguments.
+        """
+        # TODO: check whether the callback has compatible signature
 
-    # concatenate and
-    decl = ", ".join(decl_params)
-    call = ", ".join(call_params)
-    return decl, call, ctx
+        scope = Scope(self.signature.parameters.keys())
+        context = {'__builtins__': __builtins__}
+
+        filename = '<wraps(%s:%s)>' % (self.filename or '?', self.funcname)
+        callback_name = scope.reserve('_call')
+        context[callback_name] = callback
+
+        # Add a value to the context
+        empty = self.signature.empty
+        def hasattr(param, attr):
+            return not getattr(param, attr, empty) is empty
+
+        def attr(param, attr):
+            if hasattr(param, attr):
+                return ast.Attribute(
+                    value=ast.Name(id=param.name, ctx=ast.Load()),
+                    attr=attr,
+                    ctx=ast.Load())
+            else:
+                return None
+
+        params = list(self.signature.parameters.values())
+        sig = ast.arguments(
+            args=[],
+            vararg=None,
+            varargannotation=None,
+            kwonlyargs=[],
+            kwarg=None,
+            kwargannotation=None,
+            defaults=[],
+            kw_defaults=[])
+        call = ast.Call(
+            func=ast.Name(id=callback_name, ctx=ast.Load()),
+            args=[],
+            keywords=[],
+            starargs=None,
+            kwargs=None)
+
+        for param in self.signature.parameters.values():
+            context[param.name] = param
+
+            # positional parameters
+            if param.kind == param.POSITIONAL_OR_KEYWORD:
+                sig.args.append(ast_arg(
+                    arg=param.name,
+                    annotation=attr(param, 'annotation')))
+                call.args.append(ast.Name(id=param.name, ctx=ast.Load()))
+                if hasattr(param, 'default'):
+                    sig.defaults.append(attr(param, 'default'))
+
+            # keyword only
+            elif param.kind == param.KEYWORD_ONLY:
+                sig.kwonlyargs.append(ast_arg(
+                    arg=param.name,
+                    annotation=attr(param, 'annotation')))
+                call.keywords.append(ast.keyword(
+                    arg=param.name,
+                    value=ast.Name(id=param.name, ctx=ast.Load())))
+                if hasattr(param, 'default'):
+                    sig.kw_defaults.append(attr(param, 'default'))
+
+            # varargs
+            elif param.kind == param.VAR_POSITIONAL:
+                sig.vararg = param.name
+                sig.varargannotation = attr(param, 'annotation')
+                call.starargs = ast.Name(id=param.name, ctx=ast.Load())
+
+            # kwargs
+            elif param.kind == param.VAR_KEYWORD:
+                sig.kwarg = param.name
+                sig.kwargannotation = attr(param, 'annotation')
+                call.kwargs = ast.Name(id=param.name, ctx=ast.Load())
+
+            else:
+                raise ValueError("Cannot handle parameter type: %s" % param)
+
+        if hasattr(self.signature, 'return_annotation'):
+            retannot_name = scope.reserve('_returns')
+            context[retannot_name] = self.signature.return_annotation
+            returns = ast.Name(id=retannot_name, ctx=ast.Load())
+        else:
+            returns = None
+
+        # generate and evaluate the complete expression
+        loc = {}
+        if self.funcname is None:
+            expr = ast.Expression(body=ast.Lambda(
+                args=sig,
+                body=call
+            ))
+            code = compile(ast.fix_missing_locations(expr), filename, 'eval')
+            return eval(code, context, loc)
+
+        else:
+            expr = ast.Module(body=[
+                ast.FunctionDef(
+                    name=self.funcname,
+                    args=sig,
+                    body=[ast.Return(value=call)],
+                    decorator_list=[],
+                    returns=returns)
+            ])
+            code = compile(ast.fix_missing_locations(expr), filename, 'exec')
+            exec_compat(code, context, loc)
+            return loc[self.funcname]
+
+    __call__ = decorate
 
 
-def wraps(function, wrapper=None):
+    def decorate_with_boundargspec(self, function):
+        """
+        Create wrapper that calls function with a BoundArgSpec.
+        """
+        return NotImplemented
+
+    def decorate_with_boundargs(self, function):
+        """
+        Create a wrapper that calls function with a BoundArgs.
+        """
+        return self.decorate(
+            lambda *args, **kwargs:
+                function(self.signature.bind(*args, **kwargs)))
+
+
+def wraps(function=None, wrapper=None):
     """
     Wrap a function and copy its signature.
 
@@ -99,7 +182,7 @@ def wraps(function, wrapper=None):
     ... def fake(*args, **kwargs):
     ...     return "Fake: " + real(*args, **kwargs)
 
-    >>> assert getfullargspec(fake) == getfullargspec(real)
+    >>> assert signature(fake) == signature(real)
 
     >>> def check_fake(real, fake, *args, **kwargs):
     ...     rreal = real(*args, **kwargs)
@@ -118,35 +201,22 @@ def wraps(function, wrapper=None):
     # (this is for use as a decorator)
     if wrapper is None:
         return lambda wrapper: wraps(function, wrapper)
+    if function is None:
+        return lambda function: wraps(function, wrapper)
 
-    # generate signature and call expressions
-    argspec = getfullargspec(function)
-    scope = Scope(param_names(argspec))
-    sig, call, ctx = decompile_argspec(argspec, scope.reserve('_'))
-
-    # add the wrapped function to the scope
-    wrapper_name = scope.reserve('_call')
-    ctx[wrapper_name] = wrapper
-
-    # generate and evaluate the complete expression
-    never_do_this_at_home = 'lambda %s: %s(%s)' % (sig, wrapper_name, call)
-    it_is_evil = eval(never_do_this_at_home, ctx)
-
-    if hasattr(function, '__annotations__'):
-        it_is_evil.__annotations__ = function.__annotations__
-
-    # call update wrapper to copy standard attributes
-    return functools.update_wrapper(it_is_evil, function)
+    return functools.update_wrapper(
+        ASTorator.from_function(function)(wrapper),
+        function)
 
 
-def function_decorator(decorator):
+def decorator(decorator):
     """
     Decorator for signature preserving function decorators.
 
     >>> def mul(first, second = 1):
     ...     return first * second
 
-    >>> @function_decorator
+    >>> @decorator
     ... def plus_one(fn):
     ...     return lambda *args, **kwargs: 1 + fn(*args, **kwargs)
 
@@ -157,9 +227,6 @@ def function_decorator(decorator):
     """
     def decorate(function):
         return wraps(function, decorator(function))
-
-    if hasattr(decorator, '__annotations__'):
-        decorate.__annotations__ = decorator.__annotations__
 
     return functools.update_wrapper(decorate, decorator)
 
