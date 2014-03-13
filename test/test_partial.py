7b2c5c411222f6ab27e00b43ae757d07e6488a3e
Thomas Gläßle <t_glaessle@gmx.de>
2014-03-13 19:21:33 +0100

    Add more tests for `partial`

--- test/test_partial.py
+++ test/test_partial.py
@@ -6,20 +6,92 @@ __all__ = ['TestPartial']
 
 import unittest
 import functools
-from test._common import hd, _TestBase
+from test._common import _TestBase
 
 from black_magic.decorator import wraps, partial
 
 class TestPartial(unittest.TestCase, _TestBase):
 
+    def test_partial_iteration_complete(self):
+        def orig(a, b, c, d, *args, **kwargs):
+            return (a, b, c, args, kwargs)
+        part = partial(None, 'a', b='X')
+        wrap = part(orig, 'c', b='b')
+        self.assertEqual(orig('a', 'b', 'c', 'd'),
+                         wrap(d='d'))
+        self.assertEqual(orig('a', 'b', 'c', 'd', 0, e='e'),
+                         wrap('d', 0, e='e'))
+        self.assertRaises(TypeError, wrap)
+        self.assertRaises(TypeError, wrap, 'd', a='a')
+
+    def test_partial_iteration_kw_only(self):
+        def orig(a, **kwargs):
+            return (a, kwargs)
+        part = partial(None, b='X')
+        wrap = part(orig, b='b')
+        self.assertEqual(orig('a', b='b'),
+                         wrap(a='a'))
+        self.assertEqual(orig('a', b='b'),
+                         wrap('a'))
+        self.assertRaises(TypeError, wrap)
+        self.assertRaises(TypeError, wrap, 'a', 0)
+
+    def test_partial_iteration_varargs(self):
+        def orig(a, b, c, d, *args):
+            return (a, b, c, d, args)
+        part = partial(None, 'b')
+        wrap = part(orig, 'c', a='a')
+        self.assertEqual(orig('a', 'b', 'c', 'd', 0),
+                         wrap('d', 0))
+        self.assertEqual(orig('a', 'b', 'c', 'd'),
+                         wrap(d='d'))
+        self.assertRaises(TypeError, wrap)
+        self.assertRaises(TypeError, wrap, 'd', a='a')
+
+    def test_with_function_complete(self):
+        def orig(a, b, c, d, *args, **kwargs):
+            return (a, b, c, args, kwargs)
+        wrap = partial(orig, 'a', 'c', b='b')
+        self.assertEqual(orig('a', 'b', 'c', 'd', e='e'),
+                         wrap(d='d', e='e'))
+        self.assertEqual(orig('a', 'b', 'c', 'd', 0, e='e'),
+                         wrap('d', 0, e='e'))
+        self.assertRaises(TypeError, wrap)
+        self.assertRaises(TypeError, wrap, 'd', d='d')
+
+    def test_with_function_varargs(self):
+        def orig(a, b, c, d, *args):
+            return (a, b, c, args)
+        wrap = partial(orig, 'a', 'c', b='b')
+        self.assertEqual(orig('a', 'b', 'c', 'd'),
+                         wrap(d='d'))
+        self.assertEqual(orig('a', 'b', 'c', 'd'),
+                         wrap('d'))
+        self.assertEqual(orig('a', 'b', 'c', 'd', 0, 1),
+                         wrap('d', 0, 1))
+        self.assertRaises(TypeError, wrap)
+        self.assertRaises(TypeError, wrap, e='e')
+        self.assertRaises(TypeError, wrap, 'd', e='e')
+
+    def test_with_function_kwargs(self):
+        def orig(a, b, c, d, **kwargs):
+            return (a, b, c, kwargs)
+        wrap = partial(orig, 'a', 'c', b='b')
+        self.assertEqual(orig('a', 'b', 'c', 'd', e='e'),
+                         wrap(d='d', e='e'))
+        self.assertEqual(orig('a', 'b', 'c', 'd', e='e'),
+                         wrap('d', e='e'))
+        self.assertRaises(TypeError, wrap)
+        self.assertRaises(TypeError, wrap, 'd', 0)
+
     def test_with_functools_partial(self):
         def orig(a, b, c, *args, **kwargs):
             return (a, b, c, args, kwargs)
-        part = functools.partial(orig, b=1)
+        part = functools.partial(orig, b='b')
         wrap = partial(part)
-        self.assertEqual(orig(0, 1, 2, 3, 4, d=5),
-                         wrap(0, 2, 3, 4, d=5))
-        self.assertEqual(orig(0, 1, 2, d=5),
-                         wrap(0, c=2, d=5))
-        self.assertRaises(TypeError, wrap, 0, 2, b=1)
-        self.assertRaises(TypeError, wrap, 0)
+        self.assertEqual(orig('a', 'b', 'c', 0, d='d'),
+                         wrap('a', 'c', 0, d='d'))
+        self.assertEqual(orig('a', 'b', 'c', d='d'),
+                         wrap('a', c='c', d='d'))
+        self.assertRaises(TypeError, wrap, 'a', 'c', b='b')
+        self.assertRaises(TypeError, wrap, 'a')
