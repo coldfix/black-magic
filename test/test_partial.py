7209b3154334608c084e3c0c4babb078609049de
Thomas Gläßle <t_glaessle@gmx.de>
2014-03-13 18:37:53 +0100

    Fix erroneous test in test_partial.py

--- test/test_partial.py
+++ test/test_partial.py
@@ -14,11 +14,11 @@ class TestPartial(unittest.TestCase, _TestBase):
 
     def test_with_functools_partial(self):
         def orig(a, b, c, *args, **kwargs):
-            return (a, b, c)
+            return (a, b, c, args, kwargs)
         part = functools.partial(orig, b=1)
         wrap = partial(part)
         self.assertEqual(orig(0, 1, 2, 3, 4, d=5),
-                         wrap(0, 2, 3, 5, d=5))
+                         wrap(0, 2, 3, 4, d=5))
         self.assertEqual(orig(0, 1, 2, d=5),
                          wrap(0, c=2, d=5))
         self.assertRaises(TypeError, wrap, 0, 2, b=1)
