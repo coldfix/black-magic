aa4cabb72c26398fb26abb8ecd20bc085b8371d9
Thomas Gläßle <t_glaessle@gmx.de>
2014-03-21 12:59:22 +0100

    Force absolute imports

--- test/benchmark/decorator.py
+++ test/benchmark/decorator.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import decorator
 import black_magic.decorator
 
--- test/benchmark/partial.py
+++ test/benchmark/partial.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import functools
 import black_magic.decorator
 from test.benchmark import _common
