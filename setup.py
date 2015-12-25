d5003bc5ffc5928fe9188f19887f1adb2dc0084b
Thomas Gläßle <t_glaessle@gmx.de>
2015-12-25 13:53:28 +0100

    Test py34 and py35

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development',
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
     ],
